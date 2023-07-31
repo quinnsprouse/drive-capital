a7a519add1f1bb2d0159803925bcb16df5bbf358
Quinn Sprouse <quinnsprouse@gmail.com>
2023-07-30 23:24:02 -0400

    add some testing

--- src/main.js
+++ src/main.js
@@ -104,14 +104,6 @@ class NetworkAnalyzer {
   }
 }
 
-if (process.argv.length != 3) {
-  console.log("Usage: node analyze_network.js <filename>");
-  process.exit(1);
-}
-
-const analyzer = new NetworkAnalyzer();
-analyzer.analyze(process.argv[2]);
-
 module.exports = {
   Partner,
   Company,
