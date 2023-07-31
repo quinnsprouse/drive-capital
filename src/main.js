19f99597502651473b71e937d15894053e25bc1c
Quinn Sprouse <quinnsprouse@gmail.com>
2023-07-30 21:12:22 -0400

    model data 💃

---
+++ src/main.js
@@ -0,0 +1,24 @@
+class Partner {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Company {
+  constructor(name) {
+    this.name = name;
+    this.employees = new Set();
+    this.relationships = new Map();
+  }
+}
+
+class Employee {
+  constructor(name, company) {
+    this.name = name;
+    this.company = company;
+  }
+}
+
+let partners = new Map();
+let companies = new Map();
+let employees = new Map();
