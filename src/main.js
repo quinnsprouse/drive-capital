4c5eef89ea9dd51974fdc402fd7b3b4fd415a7b6
Quinn Sprouse <quinnsprouse@gmail.com>
2023-07-30 21:32:44 -0400

    add helper functions

--- src/main.js
+++ src/main.js
@@ -24,3 +24,27 @@ class Employee {
 let partners = new Map();
 let companies = new Map();
 let employees = new Map();
+
+function processPartnerCommand(name) {
+  partners.set(name, new Partner(name));
+}
+
+function processCompanyCommand(name) {
+  companies.set(name, new Company(name));
+}
+
+function processEmployeeCommand(name, companyName) {
+  if (companies.has(companyName)) {
+    const employee = new Employee(name, companyName);
+    companies.get(companyName).employees.add(employee);
+    employees.set(name, employee);
+  }
+}
+
+function processContactCommand(employeeName, partnerName) {
+  if (employees.has(employeeName) && partners.has(partnerName)) {
+    const employeeCompany = companies.get(employees.get(employeeName).company);
+    const count = employeeCompany.relationships.get(partnerName) || 0;
+    employeeCompany.relationships.set(partnerName, count + 1);
+  }
+}
